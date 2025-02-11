e10635dbd137df7b4ec01cde1e5f557e47852e75
Ubuntu <ubuntu@ip-172-31-13-80.us-east-2.compute.internal>
2025-02-11 19:33:40 +0000

    imported httpexception from fastapi

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,8 @@
 from typing import OrderedDict
 
 from fastapi import APIRouter, status
+from fastapi import HTTPException
+
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
